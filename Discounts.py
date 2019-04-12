efdd804c0370329ee91dbe5e03a7fce709ccd627
Seamus Riordan <seamus@riordan.io>
2019-04-12 15:41:36 -0500

    Start Discounts module

---
+++ Discounts.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 12, 2019
+
+@author: seamus
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
